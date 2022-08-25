26d7087ae3b131bf2cd073f2be4fcea4fc207b00
V1kt0or <103954522+V1kt0or@users.noreply.github.com>
2022-08-25 09:35:21 -0500

    Opciones del sidebar

--- lib/widgets/sidebar_menu.dart
+++ lib/widgets/sidebar_menu.dart
@@ -5,6 +5,66 @@ class sidebarMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+        child: ListView(
+      children: [
+        DrawerHeader(
+            child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: 15),
+              height: 100,
+              width: 100,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset("assets/images/avatar.jpeg"),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 10),
+              child: Text(
+                "Usuario",
+                style: TextStyle(color: Colors.black),
+              ),
+            )
+          ],
+        )),
+        ListTile(
+          leading: Icon(Icons.person),
+          title: Text(
+            "Personal",
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        ListTile(
+          leading: Icon(Icons.favorite),
+          title: Text(
+            "Whitlist",
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        ListTile(
+          leading: Icon(Icons.book),
+          title: Text(
+            "Learn",
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        ListTile(
+          leading: Icon(Icons.work),
+          title: Text(
+            "Work",
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        ListTile(
+          leading: Icon(Icons.add),
+          title: Text(
+            "Añadir categoría",
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      ],
+    ));
   }
 }

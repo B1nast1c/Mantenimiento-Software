dd42cba1f2e83b560a376eee8e90a4db01b289f4
B1nast1c <79016002+B1nast1c@users.noreply.github.com>
2022-08-27 11:43:04 -0500

    Arreglos Visuales

--- lib/widgets/sidebar_menu.dart
+++ lib/widgets/sidebar_menu.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// ignore: camel_case_types
 class sidebarMenu extends StatelessWidget {
   const sidebarMenu({Key? key}) : super(key: key);
 
@@ -8,62 +9,79 @@ class sidebarMenu extends StatelessWidget {
     return Drawer(
         child: ListView(
       children: [
-        DrawerHeader(
+        Container(
+          height: 300,
+          child: DrawerHeader(
+              child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 100),
+                height: 135,
+                width: 135,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(100.0),
+                  child: Image.asset(
+                    "assets/images/avatar.jpeg",
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 10),
+                child: Text(
+                  "Usuario",
+                  style: TextStyle(color: Colors.black),
+                ),
+              )
+            ],
+          )),
+        ),
+        Container(
+            margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+            alignment: Alignment.center,
             child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.only(top: 15),
-              height: 100,
-              width: 100,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Image.asset("assets/images/avatar.jpeg"),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 10),
-              child: Text(
-                "Usuario",
-                style: TextStyle(color: Colors.black),
-              ),
-            )
-          ],
-        )),
-        ListTile(
-          leading: Icon(Icons.person),
-          title: Text(
-            "Personal",
-            style: TextStyle(color: Colors.black),
-          ),
-        ),
-        ListTile(
-          leading: Icon(Icons.favorite),
-          title: Text(
-            "Whitlist",
-            style: TextStyle(color: Colors.black),
-          ),
-        ),
-        ListTile(
-          leading: Icon(Icons.book),
-          title: Text(
-            "Learn",
-            style: TextStyle(color: Colors.black),
-          ),
-        ),
-        ListTile(
-          leading: Icon(Icons.work),
-          title: Text(
-            "Work",
-            style: TextStyle(color: Colors.black),
-          ),
-        ),
-        ListTile(
-          leading: Icon(Icons.add),
-          title: Text(
-            "Añadir categoría",
-            style: TextStyle(color: Colors.black),
-          ),
-        ),
+              children: [
+                const ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text(
+                    "Personal",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                const ListTile(
+                  leading: Icon(Icons.favorite),
+                  title: Text(
+                    "Wishlist",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.book),
+                  title: Text(
+                    "Learn",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.work),
+                  title: Text(
+                    "Work",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                ListTile(
+                  contentPadding: EdgeInsets.only(left: 2.0),
+                  leading: IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: () => print('Nueva Categoria'),
+                  ),
+                  title: Text(
+                    "Añadir categoría",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ],
+            )),
       ],
     ));
   }

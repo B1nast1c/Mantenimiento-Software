ae2a98d3e38d03997d158940e07c6ec9176a9104
B1nast1c <79016002+B1nast1c@users.noreply.github.com>
2022-08-28 19:57:19 -0500

    Mejoras Visuales

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -5,25 +5,33 @@ class ToDo {
   String? id;
   String? todoText;
   bool isDone;
-  //String? color; //Color de la nota Este no funciona
-  Color? ncolor; // Este si
+  Color? ncolor;
 
-  ToDo({
-    required this.id,
-    required this.todoText,
-    this.isDone = false,
-    this.ncolor = Colors.white
-  });
-
+  ToDo(
+      {required this.id,
+      required this.todoText,
+      this.isDone = false,
+      this.ncolor = Colors.white});
 
   static List<ToDo> todoList() {
     return [
-      ToDo(id: '01', todoText: 'Morning Excercise', isDone: true , ncolor: verdeClaro ),
-      ToDo(id: '02', todoText: 'Buy Groceries', isDone: true, ncolor: azulClaro ),
+      ToDo(
+          id: '01',
+          todoText: 'Morning Excercise',
+          isDone: true,
+          ncolor: verdeClaro),
+      ToDo(
+          id: '02', todoText: 'Buy Groceries', isDone: true, ncolor: azulClaro),
       ToDo(id: '03', todoText: 'Check Emails', ncolor: rojoClaro),
       ToDo(id: '04', todoText: 'Team Meeting', ncolor: amarilloClaro),
-      ToDo(id: '05', todoText: 'Work on mobile apps for 2 hour', ),
-      ToDo(id: '06', todoText: 'Dinner with Jenny', ),
+      ToDo(
+        id: '05',
+        todoText: 'Work on mobile apps for 2 hour',
+      ),
+      ToDo(
+        id: '06',
+        todoText: 'Dinner with Jenny',
+      ),
     ];
   }
 }
